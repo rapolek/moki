4f14534ee96701940e9e06dcaf012099202a67a2
Rapole Krupauskaite <rkrupauskaite@gmail.com>
2023-03-16 23:05:42 +0200

    add default video

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
+import waves from './media/waves.mp4';
 
 function App() {
   return (
     <div className='App'>
+
+      <div className='Video'>
+        <video id='video-id' src={waves} controls autoplay loop muted playsinline preload='auto'></video>
+      </div>
+
       <div className='LocationButtons'>
         <button className='Button'>🌊</button>
         <button className='Button'>🌿</button>
         <button className='Button'>🗻</button>
       </div>
+
     </div>
   );
 }
