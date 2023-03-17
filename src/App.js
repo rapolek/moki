dc523845ddd5ecc1c947429150e41cadec44c0bf
Rapole Krupauskaite <rkrupauskaite@gmail.com>
2023-03-17 23:55:37 +0200

    add button functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import './App.css';
+import {useState} from 'react';
 import waves from './media/waves.mp4';
+import mountains from './media/mountains.mp4';
+import creek from './media/creek.mp4';
 
 function App() {
+
+  const [video, setVideo] = useState(waves);
+
+  const videoWaves = () => {
+    setVideo(waves);
+  }
+
+  const videoCreek = () => {
+    setVideo(creek);
+  }
+
+  const videoMountains = () => {
+    setVideo(mountains);
+  }
+
   return (
     <div className='App'>
 
       <div className='Video'>
-        <video id='video-id' src={waves} autoPlay loop muted playsInline preload='auto'></video>
+        <video id='video-id' src={video} autoPlay loop muted playsInline preload='auto'></video>
       </div>
 
       <div className='LocationButtons'>
-        <button className='Button'>🌊</button>
-        <button className='Button'>🌿</button>
-        <button className='Button'>🗻</button>
+        <button className='Button' onClick={videoWaves}>🌊</button>
+        <button className='Button' onClick={videoCreek}>🌿</button>
+        <button className='Button' onClick={videoMountains}>🗻</button>
       </div>
 
     </div>
