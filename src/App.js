7e33e0fdb66ef7809e5d4d62f901c867be2fa144
Rapole Krupauskaite <rkrupauskaite@gmail.com>
2024-04-05 18:21:56 +0300

    fix location button functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import { useState } from "react"
 import { Sidebar } from "./components/Sidebar"
-import { Background } from "./components/Background"
-import forest from "./media/forest.mp4"
 import "./App.css"
 
 function App() {
-	const [video, setVideo] = useState(forest)
-	// const video = "./media/forest.mp4"
-
 	return (
 		<div className="App">
-			<Background src={video} />
 			<Sidebar/>
 		</div>
 	)
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,18 +1,28 @@
 import { useState } from "react"
-import forest from "../media/forest.mp4"
 import LOCATIONS from "../locations"
+import { Timer } from "./Timer"
+import { Background } from "./Background"
+import forest from "../media/forest.mp4"
 
 export const Sidebar = (props) => {
+	const [video, setVideo] = useState(forest)
+
 	return (
 		<div className="Sidebar">
+			<Background src={video} />
 			<h2 className="LocationsHeading">LOCATIONS</h2>
 			<div className="LocationButtons">
 				{LOCATIONS.map((location, index) => (
-					<button className="Button" key={index} onClick={props.handleClick}>
+					<button
+						className="Button"
+						key={index}
+						onClick={() => setVideo(location.video)}
+					>
 						{location.emoji}
 					</button>
 				))}
 			</div>
+			<Timer />
 		</div>
 	)
 }
