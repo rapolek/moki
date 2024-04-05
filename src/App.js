d8442bf723402eb5594f0656d871a017614b0471
Rapole Krupauskaite <rkrupauskaite@gmail.com>
2024-04-05 17:45:08 +0300

    replace div in App.js with components Background & Sidebar

--- src/App.js
+++ src/App.js
@@ -1,55 +1,17 @@
-import "./App.css"
-import { useState, useEffect } from "react"
+import { useState } from "react"
+import { Sidebar } from "./components/Sidebar"
+import { Background } from "./components/Background"
 import forest from "./media/forest.mp4"
-import LOCATIONS from "./locations.js"
+import "./App.css"
 
 function App() {
 	const [video, setVideo] = useState(forest)
-	const [minutes, setMinutes] = useState(15)
-	const [seconds, setSeconds] = useState(0)
+	// const video = "./media/forest.mp4"
 
 	return (
 		<div className="App">
-			<div className="Sidebar">
-				<h2 className="LocationsHeading">LOCATIONS</h2>
-
-				<div className="LocationButtons">
-					{LOCATIONS.map((location, index) => (
-						<button
-							className="Button"
-							key={index}
-							onClick={() => setVideo(location.video)}
-						>
-							{location.emoji}
-						</button>
-					))}
-				</div>
-
-				<h2>TIMER</h2>
-
-				<div className="Timer">
-					<h2>
-						{minutes}:0{seconds}
-					</h2>
-
-					<div className="timer-buttons">
-						<button className="start-button">START</button>
-					</div>
-				</div>
-			</div>
-
-			<div>
-				<video
-					className="Video"
-					id="video-id"
-					src={video}
-					autoPlay
-					loop
-					muted
-					playsInline
-					preload="auto"
-				></video>
-			</div>
+			<Background src={video} />
+			<Sidebar/>
 		</div>
 	)
 }
