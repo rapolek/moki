1c7f0eb428d85ca1d66bda71866180767ac6ab59
Rapole Krupauskaite <rkrupauskaite@gmail.com>
2024-04-07 23:32:55 +0300

    remove unnecessary style attributes from Background component

--- src/components/Background.js
+++ src/components/Background.js
@@ -2,7 +2,7 @@ export const Background = (props) => {
 	return (
 		<div>
 			<video
-				className="absolute h-screen w-screen object-cover -z-10 top-0 left-0"
+				className="absolute h-screen w-screen object-cover -z-10"
 				id="video-id"
 				src={props.src}
 				autoPlay
